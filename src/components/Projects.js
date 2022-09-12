c3c4829d96eb2b17627f1fd4f4c1dd66312018c5
alysvolatile <alice.g.cooper@gmail.com>
2022-09-12 12:06:43 -0400

    Create array of portfolioProjects objects to fill out portfolio page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,36 @@
 const Projects = () => {
+
+    const portfolioProjects = [
+        {
+            name: "Counting Sheep",
+            stack: "HTML5, CSS3, Vanilla JS",
+            image: "",
+            description: "",
+            complete: true,
+        },
+        {
+            name: "Practice Helper",
+            stack: "MongoDB, Express, Liquid",
+            image: "",
+            description: "",
+            complete: true,
+        },
+        {
+            name: "",
+            stack: "",
+            image: "",
+            description: "",
+            complete: true,
+        },
+        {
+            name: "",
+            stack: "",
+            image: "",
+            description: "",
+            complete: false,
+        }
+    ]
+
     return (
         <section>
             This is the projects component.
